e2c29f556103298f8f3ade2a433d473c65a726ac
bryankaruna <bryankaruna@yahoo.com>
2019-07-08 22:16:06 +0700

    menghapus console.log pada public/chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,13 +1,11 @@
 //make connection
 var socket = io.connect('http://localhost:4000/');
-
 //Query DOM
 var message = document.getElementById('message'),
     handle = document.getElementById('handle'),
     btn = document.getElementById('send'),
     output = document.getElementById('output'),
     feedback = document.getElementById('feedback');
-
 //emmit event
 btn.addEventListener('click', function(){
     //param 1: name of message, param 2: message
@@ -16,19 +14,16 @@ btn.addEventListener('click', function(){
         handle: handle.value
     });
 })
-
-//on typing...
+//typing events
 message.addEventListener('keypress', function(){
     socket.emit('typing', handle.value);
 });
-
-//listen events
+//chat DOM
 socket.on('chat', function(data){
     feedback.innerHTML = '';
     output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
 });
-
+//typing DOM
 socket.on('typing', function(data){
-    console.log(message.textContent.length);
     feedback.innerHTML = '<p><em>' + data + ' is typing a message... </em></p>';
 });
